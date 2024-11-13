f749da1c513ee061d3894243424c25e207a6de2e
JPcGaM3 <jaitniphat@gmail.com>
2024-11-13 15:20:16 +0700

    feat(page, component): fetch data & table pagination

--- src/libs/semesters/getSemesters.tsx
+++ src/libs/semesters/getSemesters.tsx
@@ -1,7 +1,8 @@
 export default async function getSemesters() {
     try {
-        // const response = await fetch(`${process.env.PUBLIC_BACKEND_URL}api/v1/semesters?search=`)
-        const response = await fetch('http://143.198.87.246/api/v1/semesters?search=')
+        const response = await fetch(
+          `${process.env.PUBLIC_BACKEND_URL}api/v1/semesters?search=`
+        )
         if (!response.ok) {
             throw new Error(`Failed to fetch semesters ${response.statusText}`);
         }
