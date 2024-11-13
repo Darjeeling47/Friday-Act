c94cbb2ca67417494919fe9edb1e5b02857f9c66
Amphi <6633287021@student.chula.ac.th>
2024-11-13 23:17:44 +0700

    fix: application page responsive

--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -3,8 +3,12 @@
 import TableHeader from "@/components/basic/TableHeader";
 import SearchBar from "@/components/basic/SearchBar";
 import TableComponent from "@/components/basic/TableComponent";
+import { useState } from 'react';
+import { formatDate_Utc_to_EN } from "@/utils/utils";
 
 export default function Application() {
+  const [searchValue, setSearchValue] = useState('');
+
   const headers = [
     {"key" : "username", "title" : "User's Name"},
     {"key" : "sid", "title" : "Student ID"},
@@ -13,41 +17,60 @@ export default function Application() {
     {"key" : "applydate", "title" : "Apply Date"},
     {"key" : "status", "title" : "Status"},
   ]
-  const tablemock = [
-    {
-      "User's Name": "Name Surname",
-      "Student ID": "6630000021",
-      "Activity": "Activity name",
-      "Date": "20/12/2024",
-      "Apply Date": "20/11/2024",
-      "Status": "eieistatus"
-    },
-    {
-      "User's Name": "Name Surname",
-      "Student ID": "6630000021",
-      "Activity": "Activity name",
-      "Date": "20/12/2024",
-      "Apply Date": "20/11/2024",
-      "Status": "eieistatus"
-    },
-    {
-      "User's Name": "Name Surname",
-      "Student ID": "6630000021",
-      "Activity": "Activity name",
-      "Date": "20/12/2024",
-      "Apply Date": "20/11/2024",
-      "Status": "eieistatus"
-    }
-  ]
-  const handleSearchChange = (value: string) => {
-    console.log("Search Value:", value);
-    // TODO: Add search/filter logic
+  const tablemock = {
+    "data": [
+      {
+        "username": "Amphikapha Thathong",
+        "sid": "6630000021",
+        "activity": "Activity name",
+        "date": "2024-12-20T15:56:00Z",
+        "applydate": "2024-12-20T15:56:00Z",
+        "status": "eieistatus"
+      },
+      {
+        "username": "Jaitnipat Thongthawee",
+        "sid": "6630000021",
+        "activity": "Activity name",
+        "date": "2024-12-20T15:56:00Z",
+        "applydate": "2024-12-20T15:56:00Z",
+        "status": "eieistatus"
+      },
+      {
+        "username": "Jiramet Wannasiwaporn",
+        "sid": "6630000021",
+        "activity": "Activity name",
+        "date": "2024-12-20T15:56:00Z",
+        "applydate": "2024-12-20T15:56:00Z",
+        "status": "eieistatus"
+      }
+    ]
   };
-  // return
+
+  const data = tablemock.data.map(data => ({
+    ...data,
+    date: formatDate_Utc_to_EN(data.date),
+    applydate: formatDate_Utc_to_EN(data.applydate),
+  }));
+
+
+  const handleSearchChange = (value: string) => {
+    setSearchValue(value);
+    console.log("Search Value:", value);
+  };
+
+  const filteredData = tablemock.data.filter(item =>
+    item.username.toLowerCase().includes(searchValue.toLowerCase()) ||
+    item.sid.includes(searchValue) ||
+    item.activity.toLowerCase().includes(searchValue.toLowerCase()) ||
+    formatDate_Utc_to_EN(item.date).includes(searchValue) ||
+    formatDate_Utc_to_EN(item.applydate).includes(searchValue) ||
+    item.status.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
   return (
-    <main className='py-16 gap-[30px] flex flex-col'>
+    <main className='py-16 max-md:py-10 gap-[30px] flex flex-col max-md:items-center'>
       <TableHeader headerTitle='Application' buttonTitle='Scan Attendance Qr' style='flex' headerStyle='text-4xl font-semibold' buttonStyle='' />
-      <div className="flex justify-end">
+      <div className="flex justify-end max-md:justify-center">
         <SearchBar
           onChange={handleSearchChange}
           filter={[
@@ -56,7 +79,9 @@ export default function Application() {
           ]}
         />
       </div>
-      <TableComponent headers={headers} data={tablemock} />
+      {/* <div className="flex w-full"> */}
+        <TableComponent headers={headers} data={data} textStyle="max-md:text-xs" headerStyle="max-md:text-xs" />
+      {/* </div> */}
     </main>
   )
 }
