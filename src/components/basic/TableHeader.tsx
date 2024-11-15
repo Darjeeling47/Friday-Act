fe5d7873e4c97074bb5ad7a53bb0b61d593d3492
Poomrapee-chsk <poomrapeecharoensak@gmail.com>
2024-11-15 18:09:29 +0700

    feat: add option to disable header button

--- src/components/basic/TableHeader.tsx
+++ src/components/basic/TableHeader.tsx
@@ -6,24 +6,29 @@ export default function TableHeader ({
     style,
     headerStyle,
     buttonStyle,
+    disableButton,
     onClick,
     href
   } : {
     headerTitle: string,
-    buttonTitle: string,
     style?: string,
     headerStyle?: string,
     buttonStyle?: string,
     onClick?: () => void
     href?: string
-  }
+  } & (
+    | { disableButton: true; buttonTitle?: never }
+    | { disableButton?: false; buttonTitle: string }
+  )
   ) {
   return (
-    <div className={`flex max-md:flex-col items-center justify-center justify-between ${style}`}>
+    <div className={`flex max-md:flex-col ${ disableButton ? 'justify-start' : 'items-center justify-center justify-between'} ${style}`}>
       <p className={`text-2xl font-semibold ${headerStyle}`}>{headerTitle}</p>
+      { !disableButton &&
         <Button className={`max-md:mt-4 ${buttonStyle}`} onClick={onClick} href={href}>
           {buttonTitle}
         </Button>
+      }
     </div>
   )
 }
