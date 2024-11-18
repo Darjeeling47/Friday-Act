af714f6eb4994488fcbd2b737a632908ccfa41df
Poomrapee-chsk <poomrapeecharoensak@gmail.com>
2024-11-18 23:06:35 +0700

    feat: add breakpoint option for md and lg

--- src/components/table/ExpandList.tsx
+++ src/components/table/ExpandList.tsx
@@ -5,17 +5,20 @@ import ExpandCard from './ExpandCard'
 export default function ExpandList({
   title,
   children,
+  breakpoint = 'sm',
   data,
   listKey,
 }: {
   title: string
   children: (data: any) => React.ReactNode
+  breakpoint?: 'sm' | 'md'
   data: any[]
   listKey: string
 }) {
   // Return
   return (
-    <div className='flex flex-col border-t border-t-mgray-6 sm:hidden'>
+    <div
+      className={`flex flex-col border-t border-t-mgray-6 ${breakpoint == 'md' ? 'md:hidden' : 'sm:hidden'}`}>
       <p className='border-b border-b-mgray-6 py-2 pl-7 text-[15px] font-medium text-mgray-2'>
         {title}
       </p>
