6b4f6a94a7aaec7e258aedde42393da4cbabc25c
popo <yosita.rd@gmail.com>
2024-11-19 12:30:28 +0700

    fix: activity card ui

--- src/components/activities/ActivityItem.tsx
+++ src/components/activities/ActivityItem.tsx
@@ -1,53 +1,57 @@
 'use client'
 //import react
-import { useState, useEffect } from "react";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useState, useEffect } from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 // import components
-import Tag from "../basic/Tag";
+import Tag from '../basic/Tag'
 
 // import libs
-import { getImageAsBase64 } from "@/utils/getImageAsBase64";
+import { getImageAsBase64 } from '@/utils/getImageAsBase64'
 
 // import interface
-import { ActivityItem } from "@/interface/activitiesInterface";
-import { TagItem } from "@/interface/tagsInterface";
+import { ActivityItem } from '@/interface/activitiesInterface'
+import { TagItem } from '@/interface/tagsInterface'
 
 export default function ActivitiesItem({
   activity,
 }: {
-  activity: ActivityItem;
+  activity: ActivityItem
 }) {
   // Primary variables
-  const [imgSrc, setImgSrc] = useState<string | undefined>(undefined);
-  const [nameActivity, setNameActivity] = useState<string>("NaN");
-  const [nameCompany, setNameCompany] = useState<string>("NaN");
-  const [description, setDescription] = useState<string>("");
-  const router = useRouter();
+  const [imgSrc, setImgSrc] = useState<string | undefined>(undefined)
+  const [nameActivity, setNameActivity] = useState<string>('NaN')
+  const [nameCompany, setNameCompany] = useState<string>('NaN')
+  const [description, setDescription] = useState<string>('')
+  const router = useRouter()
 
   // useEffect for get image as base64
   useEffect(() => {
     const fetchImage = async () => {
-
       if (!activity.posterUrl) {
-        setImgSrc(undefined);
+        setImgSrc(undefined)
         return
       }
 
-      const imgSrc = await getImageAsBase64(activity.posterUrl);
-      setImgSrc(imgSrc);
+      const imgSrc = await getImageAsBase64(activity.posterUrl)
+      setImgSrc(imgSrc)
     }
-    fetchImage();
+    fetchImage()
 
     if (activity.name) {
       setNameActivity(activity.name)
     }
 
-    if (activity.company?.companyNameEn && activity.company?.companyNameEn.length > 0) {
+    if (
+      activity.company?.companyNameEn &&
+      activity.company?.companyNameEn.length > 0
+    ) {
       setNameCompany(activity.company.companyNameEn)
-    }
-    else if (activity.company?.companyNameTh && activity.company?.companyNameTh.length > 0) {
+    } else if (
+      activity.company?.companyNameTh &&
+      activity.company?.companyNameTh.length > 0
+    ) {
       setNameCompany(activity.company.companyNameTh)
     }
 
@@ -59,52 +63,58 @@ export default function ActivitiesItem({
   // return
   return (
     <button
-      className="flex flex-row justify-start items-center gap-5 hover:bg-mgray-3/10 p-5 rounded-[30px] w-full max-w-xl h-full transition-transform duration-300"
+      className='flex h-full w-full max-w-xl flex-row items-center justify-start gap-5 rounded-md p-5 transition-transform duration-300 hover:bg-mgray-3/10'
       onClick={(e) => {
-        e.preventDefault();
+        e.preventDefault()
         router.push(`/activity/${activity.id}`)
-      }}
-    >
-      <div className="grow-0">
-        <div className="flex justify-center items-center bg-white shadow-1 rounded-3xl h-44 sm:h-56 md:h-64 lg:h-72 aspect-2/3">
+      }}>
+      <div className='grow-0'>
+        <div className='flex aspect-2/3 h-44 items-center justify-center rounded-md bg-white shadow-1 sm:h-56 md:h-64 lg:h-72'>
           {imgSrc ? (
             <Image
               src={imgSrc}
               alt={`Activity Image ${activity.name}`}
               width={240}
               height={320}
-              className="rounded-3xl w-full h-full aspect-2/3 object-cover"
+              className='aspect-2/3 h-full w-full rounded-md object-cover'
               quality={90}
-              sizes="(max-width: 768px) 100vw, 240px"
+              sizes='(max-width: 768px) 100vw, 240px'
             />
           ) : (
             <Image
-              src="/logo/Logo_FA_Full.png"
-              alt="Activity Image"
+              src='/logo/Logo_FA_Full.png'
+              alt='Activity Image'
               width={240}
               height={320}
-              className="rounded-3xl w-3/4 aspect-2/3 object-contain"
+              className='aspect-2/3 w-3/4 rounded-md object-contain'
             />
           )}
         </div>
       </div>
-      <div className="h-full text-left overflow-hidden grow">
-        <div className="flex flex-col justify-start items-start gap-1 py-1 md:py-2 lg:py-3 w-full">
-          <div className="flex flex-col justify-start items-start gap-1 w-full">
-            <h3 className="line-clamp-1 font-normal text-mgray-1 text-subtitle">{nameActivity}</h3>
-            <p className="line-clamp-1 font-light text-detail-1 text-mgray-2">{nameCompany}</p>
+      <div className='h-full grow overflow-hidden text-left'>
+        <div className='flex w-full flex-col items-start justify-start gap-1 py-1 md:py-2 lg:py-3'>
+          <div className='flex w-full flex-col items-start justify-start gap-1'>
+            <h3 className='line-clamp-1 text-subtitle font-normal text-mgray-1'>
+              {nameActivity}
+            </h3>
+            <p className='text-detail-1 line-clamp-1 font-light text-mgray-2'>
+              {nameCompany}
+            </p>
           </div>
-          <hr className="w-full" />
-          <div className="flex flex-row justify-start items-center gap-2 mt-1 scrollbar-thumb-gray-300 px-1 w-full h-9 md:h-10 lg:h-12 overflow-x-auto scrollbar-thin scrollbar-track-gray-100">
-            {activity.tags.sort().slice(0, 3).map((tag: TagItem) => (
-              <Tag key={tag.id} text={tag.name} color={tag.color} />
-            ))}
+          <hr className='w-full' />
+          <div className='mt-1 flex h-9 w-full flex-row items-center justify-start gap-2 overflow-x-auto px-1 scrollbar-thin scrollbar-track-gray-100 scrollbar-thumb-gray-300 md:h-10 lg:h-12'>
+            {activity.tags
+              .sort()
+              .slice(0, 3)
+              .map((tag: TagItem) => (
+                <Tag key={tag.id} text={tag.name} color={tag.color} />
+              ))}
           </div>
-          <div className="line-clamp-1 sm:line-clamp-2 lg:line-clamp-3 pt-1 w-full font-light text-body-2 text-left text-mgray-2">
+          <div className='line-clamp-1 w-full pt-1 text-left text-body-2 font-light text-mgray-2 sm:line-clamp-2 lg:line-clamp-3'>
             {description}
           </div>
         </div>
       </div>
     </button>
-  );
+  )
 }
--- src/components/activities/ActivityItemApplied.tsx
+++ src/components/activities/ActivityItemApplied.tsx
@@ -1,64 +1,64 @@
 'use client'
 //import react
-import { useState, useEffect } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useState, useEffect } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 // import components
 import Tag from '../basic/Tag'
-import Button from "../basic/Button";
+import Button from '../basic/Button'
 
 // import libs
-import { getImageAsBase64 } from "@/utils/getImageAsBase64";
+import { getImageAsBase64 } from '@/utils/getImageAsBase64'
 
 // import interface
 import { ActivityItem } from '@/interface/activitiesInterface'
 import { TagItem } from '@/interface/tagsInterface'
-import { Skeleton } from "../basic/Skeleton";
+import { Skeleton } from '../basic/Skeleton'
 
 export default function ActivityItemApplied({
-  activity
+  activity,
 }: {
   activity: ActivityItem
 }) {
   // Primary variables
-  let availableSeats: number = activity.maxParticipants - activity.currentParticipants;
-  const [imgSrc, setImgSrc] = useState<string | undefined>(undefined);
-  const [nameActivity, setNameActivity] = useState<string>("NaN");
-  const [nameCompany, setNameCompany] = useState<string>("NaN");
-  const [description, setDescription] = useState<string>("");
-  const [dateActivity, setDateActivity] = useState<string>("");
-  const [timeActivity, setTimeActivity] = useState<string>("");
-  const router = useRouter();
+  let availableSeats: number =
+    activity.maxParticipants - activity.currentParticipants
+  const [imgSrc, setImgSrc] = useState<string | undefined>(undefined)
+  const [nameActivity, setNameActivity] = useState<string>('NaN')
+  const [nameCompany, setNameCompany] = useState<string>('NaN')
+  const [description, setDescription] = useState<string>('')
+  const [dateActivity, setDateActivity] = useState<string>('')
+  const [timeActivity, setTimeActivity] = useState<string>('')
+  const router = useRouter()
 
   // Function for get image as base64
   const fetchImage = async () => {
-
     if (!activity.posterUrl) {
-      setImgSrc(undefined);
+      setImgSrc(undefined)
       return
     }
 
-    const imgSrc = await getImageAsBase64(activity.posterUrl);
-    setImgSrc(imgSrc);
+    const imgSrc = await getImageAsBase64(activity.posterUrl)
+    setImgSrc(imgSrc)
   }
 
   function formatDate(date: string, start_time?: string, end_time?: string) {
-    const dateObj = new Date(date);
+    const dateObj = new Date(date)
 
     // Format the date as "9 Dec 2024"
-    const day = dateObj.getDate();
-    const month = dateObj.toLocaleString('en-US', { month: 'short' });
-    const year = dateObj.getFullYear();
+    const day = dateObj.getDate()
+    const month = dateObj.toLocaleString('en-US', { month: 'short' })
+    const year = dateObj.getFullYear()
 
     // Check if start_time and end_time are defined, if not set a default value
-    const formattedStartTime = start_time ? start_time.slice(0, 5) : '00:00';
-    const formattedEndTime = end_time ? end_time.slice(0, 5) : '00:00';
+    const formattedStartTime = start_time ? start_time.slice(0, 5) : '00:00'
+    const formattedEndTime = end_time ? end_time.slice(0, 5) : '00:00'
 
     // Combine date and time
-    setDateActivity(`${day} ${month} ${year}`);
-    setTimeActivity(`${formattedStartTime} - ${formattedEndTime}`);
+    setDateActivity(`${day} ${month} ${year}`)
+    setTimeActivity(`${formattedStartTime} - ${formattedEndTime}`)
   }
 
   // useEffect for get image as base64
@@ -67,10 +67,15 @@ export default function ActivityItemApplied({
       setNameActivity(activity.name)
     }
 
-    if (activity.company?.companyNameEn && activity.company?.companyNameEn.length > 0) {
+    if (
+      activity.company?.companyNameEn &&
+      activity.company?.companyNameEn.length > 0
+    ) {
       setNameCompany(activity.company.companyNameEn)
-    }
-    else if (activity.company?.companyNameTh && activity.company?.companyNameTh.length > 0) {
+    } else if (
+      activity.company?.companyNameTh &&
+      activity.company?.companyNameTh.length > 0
+    ) {
       setNameCompany(activity.company.companyNameTh)
     }
 
@@ -78,104 +83,115 @@ export default function ActivityItemApplied({
       setDescription(activity.description)
     }
 
-    fetchImage();
+    fetchImage()
 
-    formatDate(activity.date, activity.startTime, activity.endTime);
+    formatDate(activity.date, activity.startTime, activity.endTime)
   }, [])
 
   // return
   return (
     <Link
-      className="flex flex-row justify-start items-center gap-5 hover:bg-mgray-3/10 p-5 rounded-[30px] w-full max-w-xl h-full transition-transform duration-300"
-      href={`/activity/${activity.id}`}
-    >
-      <div className="grow-0">
-        <div className="flex justify-center items-center bg-white shadow-1 rounded-3xl h-44 sm:h-56 md:h-64 lg:h-72 aspect-2/3">
+      className='flex h-full w-full max-w-xl flex-row items-center justify-start gap-5 rounded-md p-5 transition-transform duration-300 hover:bg-mgray-3/10'
+      href={`/activity/${activity.id}`}>
+      <div className='grow-0'>
+        <div className='flex aspect-2/3 h-44 items-center justify-center rounded-md bg-white shadow-1 sm:h-56 md:h-64 lg:h-72'>
           {imgSrc ? (
             <Image
               src={imgSrc}
               alt={`Activity Image ${activity.name}`}
               width={240}
               height={320}
-              className="rounded-3xl w-full h-full aspect-2/3 object-cover"
+              className='aspect-2/3 h-full w-full rounded-md object-cover'
               quality={90}
-              sizes="(max-width: 768px) 100vw, 240px"
+              sizes='(max-width: 768px) 100vw, 240px'
             />
           ) : (
             <Image
-              src="/logo/Logo_FA_Full.png"
-              alt="Activity Image"
+              src='/logo/Logo_FA_Full.png'
+              alt='Activity Image'
               width={240}
               height={320}
-              className="rounded-3xl w-3/4 aspect-2/3 object-contain"
+              className='aspect-2/3 w-3/4 rounded-md object-contain'
             />
           )}
         </div>
       </div>
-      <div className="h-44 sm:h-56 md:h-64 lg:h-72 overflow-hidden grow">
-        <div className="flex flex-col justify-between items-between w-full h-full">
-          <div className="flex flex-col justify-start items-start gap-1 py-1 md:py-2 lg:py-3 w-full">
-            <div className="flex flex-col justify-start items-start gap-1 w-full">
-              <h3 className="line-clamp-1 font-normal text-mgray-1 text-subtitle">{nameActivity}</h3>
-              <p className="line-clamp-1 font-light text-detail-1 text-mgray-2">{nameCompany}</p>
+      <div className='h-44 grow overflow-hidden sm:h-56 md:h-64 lg:h-72'>
+        <div className='items-between flex h-full w-full flex-col justify-between'>
+          <div className='flex w-full flex-col items-start justify-start gap-1 py-1 md:py-2 lg:py-3'>
+            <div className='flex w-full flex-col items-start justify-start gap-1'>
+              <h3 className='line-clamp-1 text-subtitle font-normal text-mgray-1'>
+                {nameActivity}
+              </h3>
+              <p className='text-detail-1 line-clamp-1 font-light text-mgray-2'>
+                {nameCompany}
+              </p>
             </div>
-            <hr className="w-full" />
-            <div className="flex flex-row justify-start items-center gap-2 mt-1 scrollbar-thumb-gray-300 px-1 w-full h-9 md:h-10 lg:h-12 overflow-x-auto scrollbar-thin scrollbar-track-gray-100">
-              {activity.tags.sort().slice(0, 3).map((tag: TagItem) => (
-                <Tag key={tag.id} text={tag.name} color={tag.color} />
-              ))}
+            <hr className='w-full' />
+            <div className='mt-1 flex h-9 w-full flex-row items-center justify-start gap-2 overflow-x-auto px-1 scrollbar-thin scrollbar-track-gray-100 scrollbar-thumb-gray-300 md:h-10 lg:h-12'>
+              {activity.tags
+                .sort()
+                .slice(0, 3)
+                .map((tag: TagItem) => (
+                  <Tag key={tag.id} text={tag.name} color={tag.color} />
+                ))}
             </div>
-            <div className="line-clamp-1 md:line-clamp-2 pt-1 w-full font-light text-body-2 text-mgray-2">
-              {description.length > 0 ? description : "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
+            <div className='line-clamp-1 w-full pt-1 text-body-2 font-light text-mgray-2 md:line-clamp-2'>
+              {description.length > 0
+                ? description
+                : 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
             </div>
           </div>
-          <div className="flex flex-col gap-1 w-full">
-
-            {
-              activity.isApplied ?
-                <>
-                  <button
-                    className="bg-mgray-2 hover:bg-mgray-2/80 shadow px-4 py-2 line-clamp-1 rounded-md w-full text-center text-detail-2 text-mgray-4 md:text-detail-1"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      router.push(`/activity/${activity.id}`)
-                    }}
-                  >
-                    Applied
-                  </button>
-                  {availableSeats > 0 ? (
-                    <p className='text-right pr-2 w-full text-detail-2 text-emerald-500'>{availableSeats} Seat{availableSeats !== 1 ? "s" : ""} left</p>
-                  ) : (
-                    <p className='text-right pr-2 w-full text-detail-2 text-rose-600'>Full</p>
-                  )}
-                </>
-                : availableSeats > 0 ? (
-                  <>
-                    <button
-                      className="bg-vidva hover:bg-vidva/80 shadow px-4 py-2 line-clamp-1 rounded-md w-full text-center text-detail-2 text-mgray-4 md:text-detail-1"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        router.push(`/activity/${activity.id}`)
-                      }}
-                    >
-                      Apply Now
-                    </button>
-                    <p className='text-right pr-2 w-full text-detail-2 text-emerald-500'>{availableSeats} Seat{availableSeats !== 1 ? "s" : ""} left</p>
-                  </>
+          <div className='flex w-full flex-col gap-1'>
+            {activity.isApplied ? (
+              <>
+                <button
+                  className='shadow text-detail-2 md:text-detail-1 line-clamp-1 w-full rounded-md bg-mgray-2 px-4 py-2 text-center text-mgray-4 hover:bg-mgray-2/80'
+                  onClick={(e) => {
+                    e.preventDefault()
+                    router.push(`/activity/${activity.id}`)
+                  }}>
+                  Applied
+                </button>
+                {availableSeats > 0 ? (
+                  <p className='text-detail-2 w-full pr-2 text-right text-emerald-500'>
+                    {availableSeats} Seat{availableSeats !== 1 ? 's' : ''} left
+                  </p>
                 ) : (
-                  <>
-                    <button
-                      className="bg-mgray-3 shadow px-4 py-2 line-clamp-1 rounded-md w-full text-center text-detail-2 text-mgray-5 md:text-detail-1 cursor-not-allowed"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        router.push(`/activity/${activity.id}`)
-                      }}
-                    >
-                      Out of Seat
-                    </button>
-                    <p className='text-right pr-2 w-full text-detail-2 text-rose-600'>Full</p>
-                  </>
+                  <p className='text-detail-2 w-full pr-2 text-right text-rose-600'>
+                    Full
+                  </p>
                 )}
+              </>
+            ) : availableSeats > 0 ? (
+              <>
+                <button
+                  className='shadow text-detail-2 md:text-detail-1 line-clamp-1 w-full rounded-md bg-vidva px-4 py-2 text-center text-mgray-4 hover:bg-vidva/80'
+                  onClick={(e) => {
+                    e.preventDefault()
+                    router.push(`/activity/${activity.id}`)
+                  }}>
+                  Apply Now
+                </button>
+                <p className='text-detail-2 w-full pr-2 text-right text-emerald-500'>
+                  {availableSeats} Seat{availableSeats !== 1 ? 's' : ''} left
+                </p>
+              </>
+            ) : (
+              <>
+                <button
+                  className='shadow text-detail-2 md:text-detail-1 line-clamp-1 w-full cursor-not-allowed rounded-md bg-mgray-3 px-4 py-2 text-center text-mgray-5'
+                  onClick={(e) => {
+                    e.preventDefault()
+                    router.push(`/activity/${activity.id}`)
+                  }}>
+                  Out of Seat
+                </button>
+                <p className='text-detail-2 w-full pr-2 text-right text-rose-600'>
+                  Full
+                </p>
+              </>
+            )}
           </div>
         </div>
       </div>
