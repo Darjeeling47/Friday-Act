db19a92fc0db6f995fcef6d5f60e0a399c853d46
Poomrapee-chsk <poomrapeecharoensak@gmail.com>
2024-11-20 18:43:33 +0700

    feat: add query in api libs

--- src/libs/activities/getActivityParticipants.tsx
+++ src/libs/activities/getActivityParticipants.tsx
@@ -1,11 +1,32 @@
 import { cookies } from 'next/headers'
 
-export default async function getActivityParticipants(id?: string) {
+export default async function getActivityParticipants({
+  id,
+  search,
+  admission_year,
+}: {
+  id: string
+  search?: string
+  admission_year?: number
+}) {
   const cookieStore = cookies()
   const token = cookieStore.get('access_token')?.value
 
+  let query = ''
+  if (search || admission_year) {
+    query += '?'
+  }
+
+  if (search) {
+    query += `&search=${search}`
+  }
+
+  if (admission_year) {
+    query += `&admission_year=${admission_year}`
+  }
+
   // Construct the API endpoint URL
-  const url = `${process.env.PUBLIC_BACKEND_URL}/api/v1/activities/${id}/participants`;
+  const url = `${process.env.PUBLIC_BACKEND_URL}/api/v1/activities/${id}/participants${query}`
 
   const response = await fetch(url, {
     method: 'GET',
@@ -13,11 +34,11 @@ export default async function getActivityParticipants(id?: string) {
       Authorization: `Bearer ${token}`,
     },
     cache: 'no-cache',
-  });
+  })
 
   // Check if the response is successful
   if (!response.ok) {
-    throw new Error('get activity participants failed');
+    throw new Error('get activity participants failed')
   }
 
   // Return data
--- src/libs/activities/getActivityParticipantsClient.tsx
+++ src/libs/activities/getActivityParticipantsClient.tsx
@@ -1,11 +1,32 @@
 // Workaround for bug: "× You're importing a component that needs next/headers"
 import Cookies from 'js-cookie'
 
-export default async function getActivityParticipantsClient (id: string) {
+export default async function getActivityParticipantsClient({
+  id,
+  search,
+  admission_year,
+}: {
+  id: string
+  search?: string
+  admission_year?: number
+}) {
   const token = Cookies.get('access_token')
 
+  let query = ''
+  if (search || admission_year) {
+    query += '?'
+  }
+
+  if (search) {
+    query += `&search=${search}`
+  }
+
+  if (admission_year) {
+    query += `&admission_year=${admission_year}`
+  }
+
   // Construct the API endpoint URL
-  const url = `${process.env.PUBLIC_BACKEND_URL}/api/v1/activities/${id}/participants`;
+  const url = `${process.env.PUBLIC_BACKEND_URL}/api/v1/activities/${id}/participants`
 
   const response = await fetch(url, {
     method: 'GET',
@@ -13,11 +34,11 @@ export default async function getActivityParticipantsClient (id: string) {
       Authorization: `Bearer ${token}`,
     },
     cache: 'no-cache',
-  });
+  })
 
   // Check if the response is successful
   if (!response.ok) {
-    throw new Error('get activity participants failed');
+    throw new Error('get activity participants failed')
   }
 
   // Return data
